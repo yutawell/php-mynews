155f22b3d8c11a8222b40cc92a001c2851dbc3c8
小野優太 <bzbzbzstock@outlook.jp>
2021-02-18 01:31:02 +0000

    PHP/Laravel 10 ControllerとViewが連携できるようにしよう

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>MyProfiles</title>
+    </head>
+    <body>
+        <h1>Myプロファイル作成画面</h1>
+    </body>
+</html>
